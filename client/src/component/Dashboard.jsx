1fce7b807d581b6fc2e887fe7b09be47013740e2
tasyaangesti <tasyahestiaangesti@gmail.com>
2024-08-13 19:16:36 +0700

    feat: top up tagihan

--- client/src/component/Dashboard.jsx
+++ client/src/component/Dashboard.jsx
@@ -1,6 +1,29 @@
 import { useState } from "react";
 import Carousel from "./section/Carousel";
 import Kategori from "./section/Kategori";
+import Topup from "./section/Topup";
+
+const items = [
+  "Pulsa",
+  "Paket Data",
+  "Listrik PLN",
+  "Uang Elektronik",
+  "Air PDAM",
+];
+
+const nominalOptions = [
+  { value: "10000", label: "10.000" },
+  { value: "20000", label: "20.000" },
+  { value: "50000", label: "50.000" },
+  { value: "100000", label: "100.000" },
+];
+
+const paketDataOptions = [
+  { value: "20000", label: "Paket Seminggu - 20.000" },
+  { value: "50000", label: "Paket Sebulan - 50.000" },
+  { value: "78000", label: "Paket Bulanan OMG - 78.000" },
+  { value: "54000", label: "Paket Berkah - 54.000" },
+];
 
 export function Dashboard() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -22,7 +45,6 @@ export function Dashboard() {
 
   return (
     <div className="my-10">
-      {/* carouser + kategori */}
       <Carousel
         slides={slides}
         currentIndex={currentIndex}
@@ -31,7 +53,12 @@ export function Dashboard() {
         goToSlide={goToSlide}
       />
 
-      <Kategori/>
+      <Kategori />
+      <Topup
+        items={items}
+        nominalOptions={nominalOptions}
+        paketDataOptions={paketDataOptions}
+      />
     </div>
   );
 }
---
+++ client/src/component/section/Topup.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+
+export default function Topup({ items, nominalOptions, paketDataOptions }) {
+  const [active, setActive] = useState(items[0]);
+
+  const InputNominal = ({ placeholder }) => (
+    <div className="relative flex-1">
+      <input
+        className="border rounded-md p-2 pl-12 w-full"
+        type="tel"
+        placeholder={placeholder}
+      />
+      <img
+        src="/tel.png"
+        alt="phone icon"
+        className="absolute top-2 left-2 w-6 h-6"
+      />
+    </div>
+  );
+
+  const SelectNominal = ({ options }) => (
+    <div className="relative flex-1 pt-3 lg:-mt-3 md:-mt-3">
+      <select
+        className="border rounded-md p-2 pr-12 w-full appearance-none"
+        id="nominal"
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <img
+        src="/button-icon-arrow.png"
+        alt="arrow icon"
+        className="absolute top-[22px] right-2 w-6 h-6"
+      />
+    </div>
+  );
+
+  const handleClick = (item) => {
+    setActive(item);
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto bg-white mt-10 rounded-sm p-4">
+      {/* header */}
+      <div className="pt-3 pl-3">
+        <h1 className="text-black font-semibold">Top Up dan Tagihan</h1>
+      </div>
+
+      {/* sub title */}
+      <div className="mt-2">
+        <div className="flex items-center">
+          {items.map((item, index) => (
+            <div
+              key={item}
+              onClick={() => handleClick(item)}
+              className={`w-80 text-center border-b-2 ${
+                active === item
+                  ? "border-[#0A91AB] text-[#0A91AB]"
+                  : "border-gray-300 text-[#61646B]"
+              } ${index >= 2 ? "hidden md:block" : ""}`}
+            >
+              <h1
+                className={`font-semibold ${
+                  active === item ? "text-[#0A91AB]" : "text-[#61646B]"
+                } px-1.5 py-1.5`}
+              >
+                {item}
+              </h1>
+            </div>
+          ))}
+          <div className="md:hidden">
+            <button className="focus:outline-none">
+              <img src="/Button-Icon.png" alt="button" className="w-36 h-10" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {active === "Pulsa" && (
+        <div className="flex flex-col md:flex-row md:gap-5 mt-6 bg">
+          <InputNominal placeholder="Nomor Telepon" />
+          <SelectNominal options={nominalOptions} className="" />
+          <div className="flex items-center mt-4 md:mt-0 flex-shrink-0">
+            <button className="rounded-md bg-[#FFFFFF] border-2 border-[#0A91AB] text-[#0A91AB] font-semibold px-6 py-2 hover:shadow-lg hover:drop-shadow transition duration-200 w-full md:w-auto">
+              Beli
+            </button>
+          </div>
+        </div>
+      )}
+
+      {active === "Paket Data" && (
+        <div className="flex flex-col md:flex-row md:gap-5 mt-6">
+          <InputNominal placeholder="Nomor Telepon" />
+          <SelectNominal options={paketDataOptions} />
+          <div className="flex items-center mt-4 md:mt-0 flex-shrink-0">
+            <button className="rounded-md bg-[#FFFFFF] border-2 border-[#0A91AB] text-[#0A91AB] font-semibold px-6 py-2 hover:shadow-lg hover:drop-shadow transition duration-200 w-full md:w-auto">
+              Beli
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
